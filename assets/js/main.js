caf06bfbedaae45d9cb4c66bb14715dd4ec6837a
Gary Hughes <gary.hughes87@virginmedia.com>
2020-09-28 12:50:39 +0000

    Added variable creation, data types and operator examples

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+document.body.innerHTML = "This is some content";
+
+// String data type
+let name = 'Gary "hello" Hughes ';
+let number = "134324";
+
+// Numeric data type
+let num = -1; // Float numeric data type
+let integer = -327864.9238473;
+
+// Boolean data type
+let bool1 = true; // or = 1
+let bool2 = false; // or = 0
+
+// Undefined data
+let hey; // Haven't defined any data to the variable
+
+// Null data type
+let hey2 = null; // = Nothing
+
+console.log(name);
+
+console.log(typeof name); // Tells you in the console the data type of the variable
+
+/* Assignment Operators 
+
+=
++=
+-=
+*=
+/=
+%= 
+
+*/
+
+let number2 = 10;
+// number2 = number2 + 5;
+number2 += 5; // Add 5 to the current value of the variable
+
+/* Arithmetic Operators
++
+-
+*
+/
+
+*/
+
+let number3 = 5 * 10;
+
+/* String Operators
+++
+*/ 
+
+let age = 33;
+let info = "My name is Gary, I'm " + age + " years old. " + 10;
+console.log(info);
+
+let numbers = 4 + " " + 5; // Create space between numeric data
+console.log(numbers);
