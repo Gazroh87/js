a0ad7e64e0340a80e1bf467df8bbe239cded3ed7
Gary Hughes <gary.hughes87@virginmedia.com>
2020-09-29 15:17:46 +0000

    Added conditional statement, properties/statements, named and anonymous function javascript examples

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,12 +24,18 @@ console.log(typeof name); // Tells you in the console the data type of the varia
 
 /* Assignment Operators 
 
-=
-+=
--=
+=   Assigning a value
++=  Add something to the value
+-=  Take away from the value
 *=
 /=
-%= 
+%=  Percentage of the value
+==  Check if something is equal to the value
+!=  Check if something is not equal to the value
+>=  Value is greater than...
+<=  Value is less than...
+=== Check something for matching data type and value
+!== Check something for none matching data type or value
 
 */
 
@@ -49,11 +55,119 @@ let number3 = 5 * 10;
 
 /* String Operators
 ++
-*/ 
+*/
 
-let age = 33;
-let info = "My name is Gary, I'm " + age + " years old. " + 10;
+let myAge = 33;
+let info = "My name is Gary, I'm " + myAge + " years old. " + 10;
 console.log(info);
 
 let numbers = 4 + " " + 5; // Create space between numeric data
 console.log(numbers);
+
+// Conditional Statements
+let x = 10;
+let y = 19;
+// || = Pipe symbol, means or
+// && = Something & something
+
+if (x == 10 || y < 20) {
+  console.log("If! 1");
+}
+
+if (x == 10 && y < 20) {
+  console.log("If! 2");
+}
+
+if (x == 10 && y > 20) {
+  console.log("If! 3");
+} else {
+  console.log("Else! 1");
+}
+
+if (x == 10 && y < 20) {
+  console.log("If! 4");
+} else if (x > 50) {
+  console.log("Else if! 1");
+} else {
+  console.log("Else! 2");
+}
+
+let gender = "Male";
+let age = 26;
+
+if (gender == "Male") {
+  if (age >= 18) {
+    console.log("You are a " + gender + " and above the age limit!");
+  } else {
+    console.log("Sorry dude! You are below the age limit!");
+  }
+} else if (gender == "Female") {
+  if (age >= 18) {
+    console.log("You are a " + gender + " and above the age limit!");
+  } else {
+    console.log("Sorry lady! You are below the age limit!");
+  }
+} else {
+  console.log("Can't figure out your gender!");
+}
+
+let items = ["Bottle", "Camera", "Juice", 4, true];
+
+items[1] = 8; // Changes the value of the corresponding index number (in this case the 2nd value "Camera")
+
+console.log(items[1]);
+
+// JavaScript Properties and Methods
+
+let item = "Bottle of water";
+console.log(item.length); // Length of item variable's value
+console.log(item.indexOf("of")); // Starting index number of the value
+console.log(item.substring(7, 9)); // substring = Get part of string. Display the value starting from index number 7 and stop at 9
+console.log(item.replace("water", "Juice")); // Replace the first value with the next value
+console.log(item.toUpperCase()); // Capitalize the whole value
+
+let items2 = ["Bottle", "Camera", "Juice", 4, true];
+console.log(items2);
+let items3 = ["Bottle", "Camera", "Juice", 4, true];
+console.log(items3.length);
+let items4 = ["Bottle", "Camera", "Juice", 4, true];
+console.log(items4.join(" - ")); // Join combines all values in the array into a single string
+console.log(items4.join(", ")); // Join combines all values in the array into a single string
+console.log(items4.join(" | ")); // Join combines all values in the array into a single string
+console.log(items4.join(" & ")); // Join combines all values in the array into a single string
+console.log(items4.join(" / ")); // Join combines all values in the array into a single string
+console.log(items4.pop()); // Remove the last value in the array and display it in the console
+let items5 = ["Bottle", "Camera", "Juice", 4, true];
+items5.pop(); // Remove the last value in the array and then display the array in the console
+console.log(items5);
+let items6 = ["Bottle", 4, true];
+items6.push("Juice"); // Add the value on to the end of the array as a string
+let items7 = ["Bottle", 4, true];
+console.log(items7.push("Juice")); // Display the index number of the added value
+console.log(items6, items7); // Display the updated arrays
+
+// NAMED FUNCTION - Stored at the top level inside JavaScript code. We can use this function whenever we want inside the code
+function testExample(a,b) {
+    let greeting = "Hi! My name is " + a;
+    // console.log(greeting);
+    return greeting; // Stores the variable but does not display it in the console
+}
+
+let name2 = "Daniel";
+console.log(testExample("Nick"));
+console.log(testExample(name2));
+
+// ANONYMOUS FUNCTION - Equal to variable. The variable gets stored at the top level in the same way as the function. But we don't have the value from the function until we go down the file 
+let testExample2 = function(a) {
+     let greeting = "Hi! My name is " + a;
+    return greeting; // Stores the variable but does not display it in the console
+}
+
+let a = "Gary";
+console.log(testExample2(a));
+
+(function() {
+let greeting = "Hi! My name is Robert";
+console.log(greeting);
+}())
+
